61ee0869ca803a4b00857778c031e0bf72714be3
unknown <tania04bukhenko@gmail.com>
2024-11-23 19:13:07 +0200

    add todo app logic

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
-# React TypeScript Starter Pack
+# Todo app
+A simple Todo application built with React, JavaScript, and CSS, styled with Bulma, and enhanced with animations using React Transition Group.
 
-To use this template click `Use this template`
+## Demo
 
-### Available Scripts
 
-`Deploy` - available to deploy your application to gh-pages
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+
+## Technologies
+React: Component-based architecture for building the UI.
+JavaScript: Provides application logic.
+CSS: For custom styles.
+Bulma: A modern CSS framework for clean and responsive design.
+React Transition Group: For animations during element entry, exit, and state transitions.
+
+## Features
+1. Add Todo: Quickly add new tasks to your todo list.
+2. Mark as Completed: Toggle tasks between completed and pending states.
+3. Delete Todo: Remove tasks from the list.
+Responsive Design: The app is fully responsive and adapts to different screen sizes.
+Smooth Animations: Add and remove todos with beautiful animations provided by React Transition Group.
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/tanya-bukhenko-9898871a5/)
+
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/TetianaBukhenko/todo-app-react.git
+```
+
+Go to the project directory
+
+```bash
+  cd todo-app-react
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useContext, useMemo } from 'react';
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { todosContext } from './Store';
+import { Footer } from './components/Footer/Footer';
+import { items } from './utils/utils';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const { state } = useContext(todosContext);
+  const { todos, filter, tempTodo, updatedAt, errorMessage } = state;
+
+  const displayedTodos = useMemo(() => {
+    return items.filter(todos, filter);
+  }, [todos, filter]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {todos.length > 0 && (
+          <>
+            <TodoList
+              todos={displayedTodos}
+              updatedAt={updatedAt}
+              tempTodo={tempTodo}
+            />
+
+            <Footer />
+          </>
+        )}
+      </div>
+
+      <ErrorNotification errorMessage={errorMessage} />
     </div>
   );
 };
